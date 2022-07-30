ee29a96242308274dd8907b1f4e7703f2c396516
Frazer <sdfraxer@gmail.com>
2022-07-30 13:13:33 -0400

    initial commit

---
+++ db/migration/query/account.sql
@@ -0,0 +1,42 @@
+-- name: CreateAccount :one
+INSERT INTO accounts(
+  owner,
+  balance,
+  currency
+) values (
+  $1,$2,$3
+) RETURNING *;
+
+
+-- name: GetAccount :one
+select * from accounts
+where id = $1 LIMIT 1;
+
+-- name: GetAccountForUpdate :one
+select * from accounts
+where id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
+-- name: ListAccounts :many
+select * from accounts
+order by id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = $2
+where id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+where id = sqlc.arg(id)
+RETURNING *;
+
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts 
+where id =$1;
+
